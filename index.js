bf8c3cbc74ae9890cc621df3c30d51f77c32d0a2
hjsharkey23 <hjsharkey23@gmail.com>
2020-03-28 04:21:39 -0400

    Adds Randomize function -Adds a Randomizer function in index.js -Adds a button to run the Randomizer function -function only rearanges array of characters -it does not yet dsiplay them to the user in any way

---
+++ index.js
@@ -0,0 +1,20 @@
+const meleeCharacters = ['Dr. Mario', 'Mario', 'Luigi', 'Bowser', 'Peach', 'D.K.', 'C.Falcon','Gannondorf',
+'Falco', 'Fox', 'Ness', 'Ice Climbers', 'Kirby', 'Samus', 'Zelda', 'Sheik', 'Link', 'Young Link', 'Pichu',
+'Pikachu', 'Jigglypuff', 'Mewtwo', 'Mr.Game & Watch', 'Marth', 'Roy']
+
+
+const randomizeCharacters = function (arr) {
+    for(let i = arr.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * i)
+        const temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+      }
+      return arr
+}
+
+
+const button = document.getElementById("random")
+button.addEventListener("click", function() {
+    randomizeCharacters(meleeCharacters)
+});
