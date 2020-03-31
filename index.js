ef0289ab270f0e4fe5f8f939fe9c01ce4d173581
hjsharkey23 <hjsharkey23@gmail.com>
2020-03-30 23:59:53 -0400

    Fills divs with randomized Melee characters (text) - When clicking the randomize button, fills divs with chars name - Adds yoshi to list of character

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const meleeCharacters = ['Dr. Mario', 'Mario', 'Luigi', 'Bowser', 'Peach', 'D.K.', 'C.Falcon','Gannondorf',
+const meleeCharacters = ['Dr. Mario', 'Mario', 'Luigi', 'Bowser', 'Peach', 'Yoshi', 'D.K.', 'C.Falcon','Gannondorf',
 'Falco', 'Fox', 'Ness', 'Ice Climbers', 'Kirby', 'Samus', 'Zelda', 'Sheik', 'Link', 'Young Link', 'Pichu',
 'Pikachu', 'Jigglypuff', 'Mewtwo', 'Mr.Game & Watch', 'Marth', 'Roy']
 
@@ -15,6 +15,15 @@ const randomizeCharacters = function (arr) {
 
 const button = document.getElementById("random")
 
+const mapChars = function (arr) {
+    const divs = document.getElementsByClassName("box")
+    for(let i = 0; i < arr.length; i++) {
+        divs[i].innerHTML = meleeCharacters[i]
+    }
+}
+
 button.addEventListener("click", function() {
     randomizeCharacters(meleeCharacters)
+    mapChars(meleeCharacters)
 });
+
