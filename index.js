7889077d346effada6f70ca867465e767c096495
hjsharkey23 <hjsharkey23@gmail.com>
2020-03-31 02:43:45 -0400

    Adds character images to randomize function -Displays appropriate characters after clicking randomize -Adds new folder with all image files for characters

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const meleeCharacters = ['Dr. Mario', 'Mario', 'Luigi', 'Bowser', 'Peach', 'Yoshi', 'D.K.', 'C.Falcon','Gannondorf',
-'Falco', 'Fox', 'Ness', 'Ice Climbers', 'Kirby', 'Samus', 'Zelda', 'Sheik', 'Link', 'Young Link', 'Pichu',
-'Pikachu', 'Jigglypuff', 'Mewtwo', 'Mr.Game & Watch', 'Marth', 'Roy']
+// const meleeCharacters = ['Dr. Mario', 'Mario', 'Luigi', 'Bowser', 'Peach', 'Yoshi', 'D.K.', 'C.Falcon','Gannondorf',
+// 'Falco', 'Fox', 'Ness', 'Ice Climbers', 'Kirby', 'Samus', 'Zelda', 'Sheik', 'Link', 'Young Link', 'Pichu',
+// 'Pikachu', 'Jigglypuff', 'Mewtwo', 'Mr.Game & Watch', 'Marth', 'Roy']
+
+const meleeCharacters = ['./char-img/dr-mario.png', './char-img/mario.png', './char-img/luigi.png', './char-img/bowser.png',
+ './char-img/peach.png', './char-img/yoshi.png', './char-img/d.k.png', './char-img/c-falcon.png', './char-img/gannon.png',
+  './char-img/falco.png', './char-img/fox.png', './char-img/ness.png', './char-img/ice-climbers.png', './char-img/kirby.png',
+   './char-img/samus.png', './char-img/zelda.png', './char-img/sheik.png', './char-img/link.png', './char-img/young-link.png',
+    './char-img/pichu.png', './char-img/pikachu.png', './char-img/jigglypuff.png', './char-img/mewtwo.png',
+     './char-img/mr-game-and-watch.png', './char-img/marth.png', './char-img/roy.png']
 
 const randomizeCharacters = function (arr) {
     for(let i = arr.length - 1; i > 0; i--){
@@ -16,9 +23,9 @@ const randomizeCharacters = function (arr) {
 const button = document.getElementById("random")
 
 const mapChars = function (arr) {
-    const divs = document.getElementsByClassName("box")
+    const divs = document.querySelectorAll(".box")
     for(let i = 0; i < arr.length; i++) {
-        divs[i].innerHTML = meleeCharacters[i]
+        divs[i].innerHTML = '<img src=' + arr[i] +' />'
     }
 }
 
